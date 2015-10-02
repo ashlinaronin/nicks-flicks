0f91a3a0a52d9ea20fa23f62f6a5343f4a331fb7
Ashlin <ashlinaronin@gmail.com>
2015-10-02 10:48:32 -0700

    Hard-code some reviews for testing

--- services/ReviewsFactory.js
+++ services/ReviewsFactory.js
@@ -1,6 +1,21 @@
 flicks.factory('ReviewsFactory', function ReviewsFactory() {
     var factory = {};
-    factory.reviews = [];
+
+    // Hard-code some reviews for testing
+    factory.reviews = [
+        {
+            title: "Avatar is a classic film",
+            body: "If you like movies and/or films, you will certainly be a fan of James Cameron's latest effort. It's so good. You have to watch it. Don't not watch it or you will surely be sorry. Thank you for your time on my reviews website. Have a good day."
+        },
+        {
+            title: "Ken Watanabe's new period drama is a stinker",
+            body: "Hafstrom directs it like he's sucking too hard on a cigarette, in a quick, over-and-done-with style that results only in fitful coughs of smoke. How many times does John Cusack have to prove he is no leading man? In Shanghai he comes off as Sam Spade with chopsticks."
+        },
+        {
+            title: "Lobbyist Intrigue Bears No Witness",
+            body: "Scene after scene exists only to deliver facts for later: Carter's family background; the location of Leah's spare housekey; the presence of a mildly nosy neighbor and a clumsily introduced pet cat."
+        }
+    ];
 
 
     // title and body are both reserved words, so append "review" for safety
