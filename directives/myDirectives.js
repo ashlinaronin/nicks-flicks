89f6f6e6e51121ed6dd4ba06cae978ef0a995b08
Ashlin <ashlinaronin@gmail.com>
2015-10-02 12:35:25 -0700

    Fix nav header display

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -2,7 +2,6 @@ flicks.directive("invert", function() {
     return function (scope, element) {
         element.bind("click", function() {
             $("#nick-nav").toggleClass("navbar-inverse");
-
             $("body").toggleClass("inverted");
             $("#review-title").toggleClass("inverted");
             $("#review-body").toggleClass("inverted");
