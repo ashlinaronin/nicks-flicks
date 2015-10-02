5cc4a15ec9f2e90b4c0561b366099c26839b96b4
Ashlin <ashlinaronin@gmail.com>
2015-10-02 10:37:49 -0700

    Create app.php with state skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+var flicks = angular.module('flicks', ['ui.router']);
+
+flicks.config(function($stateProvider, $urlRouterProvider) {
+
+    // Home state doesn't need a controller bc it doesn't deal w/ data
+    $stateProvider.state('home', {
+        url: "",
+        templateUrl: "partials/home.html",
+    });
+
+    $stateProvider.state('reviews', {
+        url: "/reviews",
+        templateUrl: "partials/reviews.html",
+        controller: 'ReviewsCtrl'
+    });
+
+    $stateProvider.state('reviews.review', {
+        url: "/:reviewId",
+        templateUrl: "partials/reviews.review.html",
+        controller: 'ReviewsCtrl'
+    });
+
+
+})
